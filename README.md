e12809b13bde2b818c3bab124d0894f44561ca9c
RSLi <robertsli@163.com>
2020-02-20 00:49:29 -0500

    Add grid, ladder and complete graphs

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
 # graph-test-data
 
-Ready-to-use CSV files for testing graph/network applications and algorithms.
+Ready-to-use graph/network files in CSV for testing graph applications and algorithms.
+
+You can also tweak the generating script to generate more graphs with your specified parameters (number of nodes etc.)
+
+Feel free to make a PR to add your generated test graph.
+
+## Available Graphs
+
+Undirected Path (10, 1000, 10000 nodes)
+
+Undirected Complete Graph (10, 100, 1000 nodes)
+
+Undirected Grid (5\*5, 10\*10, 30*30 nodes)
+
+Undirected Ladder (10, 100, 1000 nodes)
+- ladders are just special cases of grids
+
+(Make a PR to add more...)
 
 ## Run the Golang Graph File Generator
 
 ```
 go run generate.go
 ```
+
+CSV files will be generated under the `gen/` directory.
--- generate.go
+++ generate.go
@@ -14,9 +14,21 @@ import (
 func main() {
 	fmt.Println("Generating graph CSV files")
 
-	EdgesToCSV(generateUndirectedPathGraph(10).Edges(), "csv/undirectedPath10.csv")
-	EdgesToCSV(generateUndirectedPathGraph(1000).Edges(), "csv/undirectedPath1000.csv")
-	EdgesToCSV(generateUndirectedPathGraph(10000).Edges(), "csv/undirectedPath10000.csv")
+	EdgesToCSV(generateUndirectedPathGraph(10).Edges(), "gen/undirectedPath10.csv")
+	EdgesToCSV(generateUndirectedPathGraph(1000).Edges(), "gen/undirectedPath1000.csv")
+	EdgesToCSV(generateUndirectedPathGraph(10000).Edges(), "gen/undirectedPath10000.csv")
+
+	EdgesToCSV(generateUndirectedGridGraph(5, 5).Edges(), "gen/undirectedGrid5x5.csv")
+	EdgesToCSV(generateUndirectedGridGraph(10, 10).Edges(), "gen/undirectedGrid10x10.csv")
+	EdgesToCSV(generateUndirectedGridGraph(30, 30).Edges(), "gen/undirectedGrid30x30.csv")
+
+	EdgesToCSV(generateUndirectedLadderGraph(10).Edges(), "gen/undirectedLadder10.csv")
+	EdgesToCSV(generateUndirectedLadderGraph(100).Edges(), "gen/undirectedLadder100.csv")
+	EdgesToCSV(generateUndirectedLadderGraph(1000).Edges(), "gen/undirectedLadder1000.csv")
+
+	EdgesToCSV(generateUndirectedCompleteGraph(10).Edges(), "gen/undirectedComplete10.csv")
+	EdgesToCSV(generateUndirectedCompleteGraph(100).Edges(), "gen/undirectedComplete100.csv")
+	EdgesToCSV(generateUndirectedCompleteGraph(1000).Edges(), "gen/undirectedComplete1000.csv")
 }
 
 // EdgesToCSV converts Gonum Graph edges into a CSV file.
@@ -29,6 +41,11 @@ func EdgesToCSV(edges graph.Edges, fileName string) {
 
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
+
+	err = writer.Write([]string{"from", "to"})
+	if err != nil {
+		log.Fatal("Cannot write to file", err)
+	}
 
 	for edges.Next() {
 		edge := edges.Edge()
@@ -50,6 +67,43 @@ func generateUndirectedPathGraph(numberOfNodes int) *simple.UndirectedGraph {
 		undirectedGraph.AddNode(curr)
 		undirectedGraph.SetEdge(undirectedGraph.NewEdge(prev, curr))
 		prev = curr
+	}
+	return undirectedGraph
+}
+
+func generateUndirectedGridGraph(r, c int) *simple.UndirectedGraph {
+	undirectedGraph := simple.NewUndirectedGraph()
+	list := []graph.Node{}
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			newNode := undirectedGraph.NewNode()
+			undirectedGraph.AddNode(newNode)
+			if j != 0 {
+				undirectedGraph.SetEdge(undirectedGraph.NewEdge(newNode, list[len(list)-1]))
+			}
+			if i != 0 {
+				undirectedGraph.SetEdge(undirectedGraph.NewEdge(newNode, list[len(list)-c]))
+			}
+			list = append(list, newNode)
+		}
+	}
+	return undirectedGraph
+}
+
+func generateUndirectedLadderGraph(numberOfRows int) *simple.UndirectedGraph {
+	return generateUndirectedGridGraph(numberOfRows, 2)
+}
+
+func generateUndirectedCompleteGraph(numberOfNodes int) *simple.UndirectedGraph {
+	undirectedGraph := simple.NewUndirectedGraph()
+	list := []graph.Node{}
+	for i := 0; i < numberOfNodes; i++ {
+		newNode := undirectedGraph.NewNode()
+		undirectedGraph.AddNode(newNode)
+		for j := 0; j < len(list); j++ {
+			undirectedGraph.SetEdge(undirectedGraph.NewEdge(newNode, list[j]))
+		}
+		list = append(list, newNode)
 	}
 	return undirectedGraph
 }
